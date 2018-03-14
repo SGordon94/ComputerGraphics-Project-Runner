130b3e0222e90966a5a01be02b1e7362a0f77a6e
Rapidfir3Pho3nix <official.rapidfir3pho3nix@gmail.com>
2018-03-14 06:38:15 -0500

    initial commit with backbone setup and square jumping with spacebar

---
+++ src/runner/application/KeyHandler.java
@@ -0,0 +1,39 @@
+package runner.application;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import com.jogamp.opengl.awt.GLJPanel;
+
+public class KeyHandler{
+	private static Runner instance;
+	private final String JUMP = "jump";
+
+	public KeyHandler(Runner runner) {
+		instance = runner.getInstance();
+
+		//uses bindings to capture key events
+		runner.getCanvas().getInputMap().put(KeyStroke.getKeyStroke("SPACE"), JUMP);
+		runner.getCanvas().getActionMap().put(JUMP, new JumpAction(runner));
+	}
+	
+	//Jump action for Player
+	class JumpAction extends AbstractAction{
+		private Runner runner;
+		JumpAction(Runner runner){
+			this.runner = runner;
+		}
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			Player player = runner.getPlayer();
+			if(player == null) return;
+			else if(!player.isJumping()) {
+				player.jump(runner);
+			}
+		}	
+	}
+}
---
+++ src/runner/application/RunnerLauncher.java
@@ -0,0 +1,8 @@
+package runner.application;
+
+public class RunnerLauncher {
+	public static void main(String[] args) {
+		Runner runner = new Runner();
+		runner.start();
+	}
+}
