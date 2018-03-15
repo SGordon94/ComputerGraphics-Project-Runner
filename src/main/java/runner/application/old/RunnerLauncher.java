8a01967cede81ab60ca0813040c1b1b3e6cd4176
Unknown <dragon.tran@ou.edu>
2018-03-14 22:59:27 -0500

    Moves legacy files to prevent build conflicts

---
+++ src/main/java/runner/application/old/RunnerLauncher.java
@@ -0,0 +1,9 @@
+//package runner.application;
+//
+//public class RunnerLauncher {
+//	public static void main(String[] args) {
+//		Runner runner = new Runner();
+//		runner.start();
+//		System.out.println("test");
+//	}
+//}
