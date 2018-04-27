4988707eac8b2ab5a5812967927536ab19c030b2
Dragon <dragon.tran@ou.edu>
2018-04-26 23:00:12 -0500

    removes legacy files

--- src/main/java/runner/application/old/RunnerLauncher.java
+++
@@ -1,9 +0,0 @@
-//package runner.application;
-//
-//public class RunnerLauncher {
-//	public static void main(String[] args) {
-//		Runner runner = new Runner();
-//		runner.start();
-//		System.out.println("test");
-//	}
-//}
